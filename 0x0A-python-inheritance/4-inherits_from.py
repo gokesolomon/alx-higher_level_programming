afbcc775c944c7585ed7d1053028a9b06a3edd07
gokesolomon <gokehsolomon@gmail.com>
2023-07-16 10:48:28 -0700

    updated

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """
-Contains the inherits_from function
+module only sub class of
 """
 
 
 def inherits_from(obj, a_class):
-    """returns true if obj is a subclass of a_class, otherwise false"""
-    return(issubclass(type(obj), a_class) and type(obj) != a_class)
+    """returns True if the object is an instance of a class
+       that inherited (directly or indirectly) from the specified class;
+       otherwise False.
+    """
+    if type(obj) != a_class:
+        return issubclass(type(obj), a_class)
+    else:
+        return False
