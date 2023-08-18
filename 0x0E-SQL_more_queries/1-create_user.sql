4fc9beba9ed9b0bceead5e0720cf78c25190dfcc
gokesolomon <gokehsolomon@gmail.com>
2023-08-17 17:48:21 -0700

    updated

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,7 @@
--- script to run msql server user
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO user_0d_1@localhost
+-- Creates the user user_0d_1 with all privileges cha.
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost';
