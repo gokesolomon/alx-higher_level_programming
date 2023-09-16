c795bcc68f4d49d0b553e919b899db733f636492
gokesolomon <gokehsolomon@gmail.com>
2023-09-16 13:45:15 -0700

    js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+    const c = 'C is fun';
+{
+    console.log(c);
+}
+const p = 'Python is cool';
+{
+    console.log(p);
+}
+const j = 'JavaScript is amazing';
+{
+    console.log(j);
+}
