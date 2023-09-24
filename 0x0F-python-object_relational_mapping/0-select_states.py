2efb0b63fb7e8e6c38793f15a6d7c3898e76df96
gokesolomon <gokehsolomon@gmail.com>
2023-09-24 09:16:59 -0700

    updated

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""it will  lists all states from the database hbtn_0e_0_usa """
+"""it will  lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
-
-
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
